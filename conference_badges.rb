5ef427467f94ee5b93c3000e7ce9142f272fdae8
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-10-03 17:24:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,15 @@
 def badge_maker(names)
   return "Hello, my name is #{names}."
 end 
+
 def batch_badge_creator(names)
   badges=[]
   names.each do |attendee|
   badges << "Hello, my name is #{attendee}."
-  
 end 
 badges
+end 
+
+def assign_rooms(attendees)
+
 end 
